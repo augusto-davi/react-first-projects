0662c0187c16193c57483ec07254dee88ff03b87
Davi <obrigatoriopcional@gmail.com>
2022-08-18 22:16:39 -0300

    add PhotoColumn component in App

--- cat-api/src/App.jsx
+++ cat-api/src/App.jsx
@@ -1,7 +1,15 @@
+import Column from './components/Column';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Column tag='Friends,happy' />
+      <Column tag='cuddle,gamer' />
+      <Column tag='playful,christmas,play' />
+      <Column tag='closeup,kiss,car' />
+    </div>
+  );
 }
 
 export default App;
